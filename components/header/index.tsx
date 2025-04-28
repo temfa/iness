35369d86e9e90ff504efc49ea94a23cdd97c0b39
temfa <akinfetemitope5@gmail.com>
2025-04-28 20:34:48 +0100

    Done

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,24 +1,41 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import Link from "next/link";
 import Layout from "@/layout";
 import Image from "next/image";
+import Bars from "@/svgs/bars";
+import Close from "@/svgs/close";
 
 export const Header = () => {
+  const [mobile, setMobile] = useState(false);
   return (
     <header className={styles.container}>
       <Layout>
-        <div className={styles.wrapper}>
+        <div className={mobile ? `${styles.wrapper} ${styles.mobile}` : styles.wrapper}>
           <div className={styles.logo}>
             <Image src="/images/logo.jpg" width={200} height={50} alt="Logo" />
+            {mobile ? <Close action={() => setMobile(false)} /> : <Bars action={() => setMobile(true)} />}
           </div>
           <nav>
-            <Link href="/">Home</Link>
-            <Link href="/music">Music</Link>
-            <Link href="/webshop">Webshop</Link>
-            <Link href="/vip">Vip</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
+            <Link onClick={() => setMobile(false)} href="/">
+              Home
+            </Link>
+            <Link onClick={() => setMobile(false)} href="/music">
+              Music
+            </Link>
+            <Link onClick={() => setMobile(false)} href="/webshop">
+              Webshop
+            </Link>
+            <Link onClick={() => setMobile(false)} href="/vip">
+              Vip
+            </Link>
+            <Link onClick={() => setMobile(false)} href="/about">
+              About
+            </Link>
+            <Link onClick={() => setMobile(false)} href="/contact">
+              Contact
+            </Link>
           </nav>
         </div>
       </Layout>
